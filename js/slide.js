7bb009a2a67e64507f09b8dfcf92a1ca3a1a1579
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-01-29 16:13:27 -0300

    removido for

--- js/slide.js
+++ js/slide.js
@@ -70,28 +70,13 @@ let API_KEY = '35d85489e2e98217e6bb80e10bd639e3';
    });
    
    let data = await response.json();
-   let dataJson = data["total_pages"]
+   let dataJson = data["results"]
    //console.log(data);
    //console.log(dataJson)
 
    let slideHeader = document.querySelector('.swiper-wrapper')
 
-   let lista = []
-   let resultadoFinal = [];
-
-   // FUNÇÃO QUE LISTA TODOS AS INFORMAÇÕES DO JSON
-   for(let i = 1; i < dataJson; i++) {
-      let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing/?api_key=${API_KEY}&page=${i}&language=pt-BR`, { 
-        method: "GET",
-        headers: headersList
-      });
-      
-      let data = await response.json();
-      let dataJson = data['results']
-
-      let resultado = [];
       dataJson.forEach((element, index, array) => {
-        lista.push(element)
         let idFilme = element.id;
         let tituloPtBr = element.title;
         let sinopsePtBr = element.overview
@@ -117,97 +102,58 @@ let API_KEY = '35d85489e2e98217e6bb80e10bd639e3';
           <div class="swiper-slide">
             <img class="img-destaque swiper-slide" src="https://image.tmdb.org/t/p/original/${bannerImage}" title="${tituloPtBr}" alt="${tituloPtBr}">
           </div>`
-          let infoSlide = document.querySelector('.swiper-wrapper')
-          infoSlide.innerHTML += `
-          <div class="detalhes-destaque">
-          <h1>${tituloPtBr}</h1>
-          <button class="btnInfo">Assistir</button>
-          <button class="btnFavoritos">+</button>
-          <div class="info-detalhes">
-            <img src="./assets/images/destaque/12 1.png" alt="Indicação 12 Anos">
-            <img src="./assets/images/destaque/4K 1.png" alt="Resolução 4K">
-            <span>${tempoDeConteudo} min</span>
-            <span>${anoLancamento}</span>
-          </div>
-        </div>`
+        //   let infoSlide = document.querySelector('.swiper-wrapper')
+        //   infoSlide.innerHTML += `
+        //   <div class="detalhes-destaque">
+        //   <h1>${tituloPtBr}</h1>
+        //   <button class="btnInfo">Assistir</button>
+        //   <button class="btnFavoritos">+</button>
+        //   <div class="info-detalhes">
+        //     <img src="./assets/images/destaque/12 1.png" alt="Indicação 12 Anos">
+        //     <img src="./assets/images/destaque/4K 1.png" alt="Resolução 4K">
+        //     <span>${tempoDeConteudo} min</span>
+        //     <span>${anoLancamento}</span>
+        //   </div>
+        // </div>`
         } if (idFilme == 76600) {
           console.log('ID Filme Meio:', idFilme)
           slideHeader.innerHTML += `
           <div class="swiper-slide">
             <img class="img-destaque swiper-slide" src="https://image.tmdb.org/t/p/original/${bannerImage}" title="${tituloPtBr}" alt="${tituloPtBr}">
           </div>`
-          let infoSlide = document.querySelector('.swiper-wrapper')
-          infoSlide.innerHTML += `
-          <div class="detalhes-destaque">
-          <h1>${tituloPtBr}</h1>
-          <button class="btnInfo">Assistir</button>
-          <button class="btnFavoritos">+</button>
-          <div class="info-detalhes">
-            <img src="./assets/images/destaque/12 1.png" alt="Indicação 12 Anos">
-            <img src="./assets/images/destaque/4K 1.png" alt="Resolução 4K">
-            <span>${tempoDeConteudo} min</span>
-            <span>${anoLancamento}</span>
-          </div>
-        </div>`
+        //   let infoSlide = document.querySelector('.swiper-wrapper')
+        //   infoSlide.innerHTML += `
+        //   <div class="detalhes-destaque">
+        //   <h1>${tituloPtBr}</h1>
+        //   <button class="btnInfo">Assistir</button>
+        //   <button class="btnFavoritos">+</button>
+        //   <div class="info-detalhes">
+        //     <img src="./assets/images/destaque/12 1.png" alt="Indicação 12 Anos">
+        //     <img src="./assets/images/destaque/4K 1.png" alt="Resolução 4K">
+        //     <span>${tempoDeConteudo} min</span>
+        //     <span>${anoLancamento}</span>
+        //   </div>
+        // </div>`
         } if (idFilme == 361743) {
           console.log('ID Filme Direita:', idFilme)
           slideHeader.innerHTML += `
           <div class="swiper-slide">
             <img class="img-destaque swiper-slide" src="https://image.tmdb.org/t/p/original/${bannerImage}" title="${tituloPtBr}" alt="${tituloPtBr}">
           </div>`
-          let infoSlide = document.querySelector('.swiper-wrapper')
-          infoSlide.innerHTML += `
-          <div class="detalhes-destaque">
-          <h1>${tituloPtBr}</h1>
-          <button class="btnInfo">Assistir</button>
-          <button class="btnFavoritos">+</button>
-          <div class="info-detalhes">
-            <img src="./assets/images/destaque/12 1.png" alt="Indicação 12 Anos">
-            <img src="./assets/images/destaque/4K 1.png" alt="Resolução 4K">
-            <span>3:12 min</span>
-            <span>${anoLancamento}</span>
-          </div>
-        </div>`
+        //   let infoSlide = document.querySelector('.swiper-wrapper')
+        //   infoSlide.innerHTML += `
+        //   <div class="detalhes-destaque">
+        //   <h1>${tituloPtBr}</h1>
+        //   <button class="btnInfo">Assistir</button>
+        //   <button class="btnFavoritos">+</button>
+        //   <div class="info-detalhes">
+        //     <img src="./assets/images/destaque/12 1.png" alt="Indicação 12 Anos">
+        //     <img src="./assets/images/destaque/4K 1.png" alt="Resolução 4K">
+        //     <span>3:12 min</span>
+        //     <span>${anoLancamento}</span>
+        //   </div>
+        // </div>`
         }
-
-
-
-
-        resultado.push({idFilme, tituloPtBr, sinopsePtBr, bannerImage, posterImage});
       });
-      //resultadoFinal.push(...resultado);      
-      //console.log(data);
-      //console.log(dataJson)
-    }
-
-    //console.log(resultadoFinal)
-
-    //resultadoFinal.forEach((element, index) => {
-      //console.log(element.tituloPtBr)
-      // let slideHeader = document.querySelector('.swiper-wrapper')
-      // let tituloAltSlide01 = element.tituloPtBr
-      // let tituloAltSlide02 = element.tituloPtBr
-      // let tituloAltSlide03 = element.tituloPtBr
-      // let bannerImage = bannerImage
-      // tituloAltSlide01 = 'Top Gun: Maverick'
-      // tituloAltSlide02 = 'Avatar: O Caminho da Água'
-      // tituloAltSlide03 = 'M3GAN'
-
-
-
-    // let slideHeader = document.querySelector('.swiper-wrapper');
-    // slideHeader.addEventListener('mouseover', () => {
-    //   setTimeout(
-    //     console.log(slideHeader),5000
-    //   )
-      
-    // })
-
-
-  // console.log(lista)
-  //  dataJson.forEach((element, index) => {
-  //   console.log(element.id, element.original_title)
-  //  });
-
 })
 ();
