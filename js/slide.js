c6f87e6772f474a0305049a015112c459f0feec2
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-02-03 08:12:53 -0300

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -121,7 +121,7 @@ const API_KEY = '35d85489e2e98217e6bb80e10bd639e3';
 
                   slideHeader.innerHTML += `
                   <div class="swiper-slide">
-                    <img class="img-destaque swiper-slide" src="https://image.tmdb.org/t/p/original/${bannerImage}" title="${tituloPtBr}" alt="${tituloPtBr}">
+                    <img class="img-destaque swiper-slide" src="https://image.tmdb.org/t/p/original/${bannerImage}" title=${tituloPtBr} alt="${tituloPtBr}">
                     <div class="detalhes-destaque">
                     <h1>${tituloPtBr}</h1>
                     <button class="btnInfo" movie-title=${tituloPtBr} movie-id=${idFilme} movie-key=${videoKey} overview="${sinopsePtBr}">Assistir</button>
