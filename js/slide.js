6844458e8e6dc1182b8ef9c727da9d566d542eb8
Jean Carlos <jean@naxostelecom.com.br>
2023-02-03 15:18:44 -0300

    adicionado loading

--- js/slide.js
+++ js/slide.js
@@ -132,3 +132,13 @@ const API_KEY = '35d85489e2e98217e6bb80e10bd639e3';
       });
 })
 ();
+
+var i = setInterval(function () {
+    
+  clearInterval(i);
+
+  // O código desejado é apenas isto:
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("conteudo").style.display = "inline";
+
+}, 4000);
